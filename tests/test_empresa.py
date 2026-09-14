c22e5ddda0846874f2806884202011811104ed59
alonso astete tapia <alonso.joaquin.at@gmail.com>
2026-09-14 20:58:23 -0300

    Update test_empresa.py

--- tests/test_empresa.py
+++ tests/test_empresa.py
@@ -5,34 +5,34 @@ from app.domain.company import Company
 
 def test_create_company_valid():
     company = Company.create(
-        companyName="Acme Ltda",
+        companyName="Compania Generica",
         rut="12345678-9",
-        email="contacto@acme.cl",
+        email="contacto@gmail.cl",
         phone="+56912345678",
-        address="Av. Siempre Viva 123",
+        address="Av. Alemania 123",
     )
 
-    assert company.companyName == "Acme Ltda"
+    assert company.companyName == "Compania Generica"
     assert company.rut == "12345678-9"
 
 
 def test_create_company_invalid_rut_raises_error():
     with pytest.raises(ValueError):
         Company.create(
-            companyName="Acme Ltda",
+            companyName="Compania Generica",
             rut="123456789",  # sin guion
-            email="contacto@acme.cl",
+            email="contacto@gmail.cl",
             phone="+56912345678",
-            address="Av. Siempre Viva 123",
+            address="Av. Alemania 123",
         )
 
 
 def test_create_company_invalid_email_raises_error():
     with pytest.raises(ValueError):
         Company.create(
-            companyName="Acme Ltda",
+            companyName="Compania Generica",
             rut="12345678-9",
             email="correo-invalido",
             phone="+56912345678",
-            address="Av. Siempre Viva 123",
+            address="Av. Alemania 123",
         )
